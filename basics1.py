6a3f73cc27c004aff8dacfb13d0a9a6e17350431
Prince Gandhi <gandhiprince2540@gmail.com>
2025-07-01 09:06:08 +0530

    Complete Basics1 code

--- basics1.py
+++ basics1.py
@@ -38,6 +38,10 @@
 # counter *= 2
 # print(counter)
 
+## Boolean
+# print(bool('True'))
+# print(bool('False'))
+
 ## Strings
 # a = 'Hello'
 # b = "World"
@@ -70,7 +74,7 @@
 # amount = 50
 # print(f'Hello {name}, your balance is {amount}.')
 
-## String Index. String Slicing
+## String Indexes and Slicing
 # python = 'I am PYHTON'
 # print(python[1:4])
 # print(python[1:])
@@ -82,23 +86,27 @@
 # print(python[-3:])
 # print(python[::-1])
 
+## String Functions
+# a = 'Hello'
+# print(len(a))
+
 ## String methods
-# quote = 'to be or not to be'
+# quote = 'to BE or not to BE'
 # print(quote.upper())
 # print(quote.lower())
 # print(quote.capitalize())
-# print(quote.find('be'))
-# print(quote.replace('be','me'))
+# print(quote.find('BE'))
+# print(quote.replace('BE','ME'))
 # print(quote)
-# quote = quote.replace('be','me')
+# quote = quote.replace('BE','ME')
 # print(quote)
 
-## Exercise
-# year = int(input ('What year were you born? '))
-# age = 2025 - year
+## Exercise for Strings
+# year = input('What year were you born? ')
+# age = 2025 - int(year)
 # print(f'Your age is {age}')
 
-## Exercise
+## Exercise for Strings
 # name = input('What is your name? ')
 # password = input('What is your password? ')
 # pass_len = len(password)
@@ -121,19 +129,68 @@
 ##
 # amazon_cart = ['toys','pens','books']
 # amazon_cart[0] = 'laptop'
-# new_cart = amazon_cart 
-# #OR new_cart = amazon_cart[:]
+# new_cart = amazon_cart # new_cart = amazon_cart[:]
 # new_cart[0] = 'gum'
 # print(new_cart)
 # print(amazon_cart)
-## Write a program to print oranges from the below list basket
+
+## Exercise for Matrix (Write a program to print oranges from the below list basket)
 # basket = ["Banana", ["Apples", ["Oranges"], "Blueberries"]];
 # print(basket[1][1][0])
 
+## List Functions
+# basket = [1,2,3,4,5]
+# print(len(basket))
+
+## List Methods
+## Adding methods. These methods do not return any value. They just modify the list.
+# basket = [1,2,3,4,5]
+# basket.append(10)
+# basket.insert(3,100)
+# basket.extend([11,12,13])   # .extent() takes and iterable
+# print(basket)
+##
+# basket = [1,2,3]
+# basket.extend('hello')
+# print(basket)
+## Removing methods. These methods do not return any value (except .pop()). They just modify the list.
+# basket = [1,2,3,4,5]
+# basket.pop()
+# basket.pop(1)
+# basket.remove(3)
+# print(basket)
+# basket.clear()
+# print(basket)
+##
 # basket = [1,2,3,4,5,3]
 # basket.remove(3)
 # print(basket)
+## index, count and in methods. These methods do not modify the list. They just return a value.
+# basket = ['a','b','c','d','e','d']
+# print(basket.index('c'))           # .index(value) - returns the index of the given value
+# print(basket.index('e',1,5))       # .index(value,start,stop) - returns the index of the given value in given range of index
+# print('a' in basket)               # in - returns a boolean value. Checks if the given value exists in the list or not
+# print(basket.count('d'))           # .count(value) - returns the number of times the given value exists in the list
+## sort, copy and reverse methods.
+# basket = ['a','b','c','d','e','d']
+# basket.sort()                      # .sort() - Modifies the list. Does not return any value. Works for a list with only string or integer as objects.
+# print(basket)
+# basket.reverse()                   # .reverse() - Modifies the list. Does not return any value. Reversing can be done using list slicing too (which does not modify the list).
+# print(basket)
+# new_basket = basket.copy()         # Returns value. Does not modify the list. Copying can be done using list slicing too.
+## sorted function
+# basket = ['a','b','c','d','e','d']
+# print(sorted(basket))              # sorted() - returns a value  but does not modify the list.
+# print(basket)
+## join method
+# space = ' '
+# sentence = space.join(['hi','my','name','is','JOJO'])     # .join(iterable) - returns the iterable joined by the given string. Hence, it is a string method.
+# print(sentence)
+## range
+# print(list(range(10)))     # range(stop)
+# print(list(range(1,10)))   # range(start,stop)
 
+## Exercise for Lists
 # basket = ["Banana", "Apples", "Oranges", "Blueberries"]
 # basket.remove('Banana')
 # basket.remove('Blueberries')
@@ -144,7 +201,7 @@
 # basket.clear()
 # print(basket)
 
-## Exercise (print a sorted list of all of our friends)
+## Exercise for Lists (add Stanley and print a sorted list of all of our friends)
 # friends = ['Simon', 'Patty', 'Joy', 'Carrie', 'Amira', 'Chu']
 ## Solution 1
 # friends.append('Stanley')
@@ -154,21 +211,133 @@
 # new_friend = ['Stanley']
 # friends.extend(new_friend)
 # print(sorted(friends))
+## Solution 3
+# new_friend = 'Stanley'
+# friends.append(new_friend)
+# friends.sort()
+# print(friends)
 
-## Exercise
-# user_profile = {
-#     'age' : 25,
-#     'username' : 'King',
-#     'weapons' : 10,
-#     'is_active' : True,
-#     'clan' : 'Tiger'
-#     }
-# print(user_profile.keys())
-# user_profile['weapons'] += 1
-# user_profile.get('is_banned', False)
-# user_profile.update({'is_banned':True})
-# user_2 = user_profile.copy()
-# user_2.update({'username':'Queen'})
-# user_2.update({'age':24})
-# print(user_profile)
+## List unpacking
+# a,b,c,*d,e,f = [1,2,3,4,5,6,7,8,9,10]
+# print(a)
+# print(b)
+# print(c)
+# print(d)
+# print(e)
+# print(f)
+
+## Dictionary
+# user = dict(age=30,username='King')
+# print(user)
+
+## Dictionary Methods
+# user = {
+#   'age':30,
+#   'username':'King',
+#   'weapons':None,
+#   'is_active':True,
+#   'clan':'Tigers'
+# }
+# print(user['age'])
+# print(user.get('password'))
+# print(user.get('password','secret'))
+# print('King' in user)
+# print('age' in user.keys())
+# print('King' in user.values())
+# print(user.items())
+# user_2 = user.copy()
+# user.clear()
+# print(user)
+# print(user_2.pop('age'))   # returns value of the key and pops it out
 # print(user_2)
+# print(user_2.popitem())    # returns the last item (both key and value) and pops it out
+# print(user_2)
+# user_2.update({'outfit':'black','weapons':'gun'})
+# print(user_2)
+
+## Exercise for dictionary
+# user = {
+#   'age':30,
+#   'username':'King',
+#   'weapons':None,
+#   'is_active':True,
+#   'clan':'Tigers'
+# }
+## SOLUTION 1
+# print(user.keys())
+# user.update({'weapons':'gun'})
+# user.update({'is_banned':False})
+# user.update({'is_banned':True})
+# user_2 = user.copy()
+# user_2.update({'username':'Queen','age':20})
+# print(user)
+# print(user_2)
+## SOLUTION 2
+# print(user.keys())
+# user['weapons'] = 'gun'
+# user.update({'is_banned': False})
+# user['is_banned'] = True
+# user2 = user.copy()
+# user2.update({'username':'Queen','age':20})
+# print(user)
+# print(user2)
+
+## Tuples
+# my_tuple = (1,2,3,4)
+# print(my_tuple[1])
+# print(5 in my_tuple)
+# new_tuple = my_tuple[1:3]
+# print(new_tuple)
+
+## Tuple Methods
+# my_tuple = (1,2,3,4,3)
+# print(my_tuple.count(3))
+# print(my_tuple.index(2))
+
+## Tuple functions
+# my_tuple = (1,2,3,4,3)
+# print(len(my_tuple))
+
+## Sets
+# my_set = {1,2,3,4,5,3}
+# print(my_set)
+
+## Set Methods
+# my_set = {1,2,3,4,5,3}
+# my_set.add(10)
+# print(my_set)
+# print(2 in my_set)
+# new_set = my_set.copy()
+# my_set.clear()
+# print(my_set)
+# print(new_set)
+## Union, intersection, difference, difference update, discard
+# my_set = {1,2,3,4,5}
+# your_set = {4,5,6,7,8,9,10}
+# print(my_set.union(your_set))            # print(my_set | your_set)
+# print(my_set.intersection(your_set))
+# print(my_set.difference(your_set))       # print(my_set & your_set)
+# print(my_set)
+# my_set.difference_update(your_set)
+# print(my_set)
+# my_set.discard(3)
+# print(my_set)
+## Disjoint
+# my_set = {1,2,3,4,5}
+# your_set = {4,5,6,7,8,9,10}
+# print(my_set.isdisjoint(your_set))       # checks if the two sets are unique
+## Subset and Superset
+# my_set = {4,5}
+# your_set = {4,5,6,7,8,9,10}
+# print(my_set.issubset(your_set))
+# print(my_set.issuperset(your_set))
+# print(your_set.issuperset(my_set))
+
+## Set Functions
+# my_set = {1,2,3,4,5,3}
+# print(len(my_set))
+
+## Exercise for sets (print a list of students who are absent)
+# school = {'Bobby','Tammy','Jammy','Sally','Danny'}
+# attendance_list = ['Jammy', 'Bobby', 'Danny', 'Sally']
+# print(school.difference(attendance_list))                  # attendance_list automatically gets converted into a set
